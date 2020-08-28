1e42712eb72bbd72338fd02c8804048eca950984
Zethson <lukas.heumos@posteo.net>
2020-08-28 13:28:29 +0200

    Bump version from 0.1.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/zethson/lcep-package',
-    version='0.1.0',
+    url='https://github.com/mlf-core/lcep-package',
+    version='1.0.0',
     zip_safe=False,
 )
